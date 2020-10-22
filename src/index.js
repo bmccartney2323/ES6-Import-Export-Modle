ecc4f44ac0789ea532c01994073e3c7ad4073f11
bmccartney2323 <65781099+bmccartney2323@users.noreply.github.com>
2020-10-22 14:58:56 -0400

    More practice

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import pi,{doublePi, triplePi} from "./math.js";
+import pi, {doublePi, triplePi} from "./math.js";
 
 ReactDOM.render(
   <ul>
